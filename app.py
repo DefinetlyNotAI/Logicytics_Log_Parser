c3e7c4ec5e9b04c44663f0d55c5d716fb9ae99e0
DefinetlyNotAI <Nirt_12023@outlook.com>
2024-09-26 10:10:30 +0400

    Added gh pages support - testing

--- app.py
+++ app.py
@@ -104,5 +104,4 @@ def upload_file():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=os.getenv('PORT', 5000))
